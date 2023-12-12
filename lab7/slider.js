e96d43b5f5c8b80a22a17b5c7059cd2e623f1b7d
dimkalu46i <28112002dd@gmail.com>
2023-12-12 12:20:55 +0300

    rework lab-7

--- lab7/slider.js
+++ lab7/slider.js
@@ -1,29 +1,28 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // инициализируем Swiper
-    var mySwiper = new Swiper('.swiper-container', {
-      // кол-во слайдов на странице
-      slidesPerView: 1,
+// инициализируем Swiper
+var mySwiper = new Swiper('.swiper-container', {
+  // кол-во слайдов на странице
+  slidesPerView: 1,
 
-      // расстояние между слайдами
-      spaceBetween: 10,
+  // расстояние между слайдами
+  spaceBetween: 10,
 
-      // указываем стрелки
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
+  loop: false,
+  // указываем стрелки
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
-      // пагинация указывает текущее местоположение на слайдаре
-      pagination: {
-        el: '.swiper-pagination',
-        // буллеты
-        clickable: true,
-        // добавление эффекта для буллетов
-        dynamicBullets: true,
-        // добавление номера текущей странице в слайдере
-        renderBullet: function (index, className) {
-            return '<span class="' + className + '">' + (index + 1) + '</span>';
-        }
-      },
-    });
-  });
+  // пагинация указывает текущее местоположение на слайдаре
+  pagination: {
+    el: '.swiper-pagination',
+    // буллеты
+    clickable: true,
+    // добавление эффекта для буллетов
+    dynamicBullets: true,
+    // добавление номера текущей странице в слайдере
+    renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + '</span>';
+    }
+  },
+});
